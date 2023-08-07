d90557cd3336e418ead9bf6d9dffb724093b8fc5
Onufriieva <onufrieva.julia@gmail.com>
2023-08-07 15:13:41 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
-import { BrowserRouter } from "react-router-dom";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/app/App';
-
+import { App } from 'components/App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-      <BrowserRouter basename="/events-planner">
-       <App />
-      </BrowserRouter>
-     
-    </React.StrictMode>
-  );
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
