d2f84276ab4ee00c57ad9ef5b8a3e36ce421dc44
Onufriieva <onufrieva.julia@gmail.com>
2023-08-08 15:23:42 +0300

    test

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -6,45 +6,6 @@ import { nanoid } from 'nanoid';
 
 
 
-// const Home = ({events, onClick}) => {
-   
-//     return (
-    
-//   <div>
-//     <h1>Events</h1>
-//     <ul>
-          
-//      {events.length > 0 &&
-//         events.map(({ name, data, time, place, description, id, category, picture, priority }) => {
-//           return (
-//             <li key={id}>
-//                 <div>
-                   
-//                 </div>
-//                 <div>
-//                     <p>{name}</p>
-//                     <p>{data}</p>
-//                     <p>{time}</p>
-//                     <p>{place}</p>
-//                     <p>{description}</p>
-//                     <p>{category}</p>
-//                     <img src={picture} alt="event" />
-//                     <p>{priority}</p>
-//                 </div>              
-//                 <button type='button' onClick={() => onClick(id)}>Delete</button> 
-//             </li>
-//           );
-//         })} 
-  
-//     </ul>
-
-//   </div>
-//   );
-//  }  
- 
-// export default Home 
-
-
 export class Home extends Component {
   state = {
       events: [   
